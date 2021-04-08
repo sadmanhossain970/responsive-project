4fba4371ba3df94de1503bd10c342d903e4c81a9
sadmanhossain970 <81827585+sadmanhossain970@users.noreply.github.com>
2021-04-08 15:27:55 +0600

    adden html css and js file for a project

---
+++ javascript.js
@@ -0,0 +1,18 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector('header').style.top = "0";
+  } else {
+    document.querySelector('header').style.top = "-50%";
+  }
+  prevScrollpos = currentScrollPos;
+};
+//menu icon bar
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
